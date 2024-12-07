118ae2fa5f740d50a4de3110dfd8bf068424ed07
EddieDevine <edevine2025@cchsdons.com>
2024-12-06 20:22:40 -0800

    Add files via upload
    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        StartingScreen.create();
+    }
+}
