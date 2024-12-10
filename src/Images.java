f89e383b78717f9742be4c5dd228de4d7a6142de
EddieDevine <edevine2025@cchsdons.com>
2024-12-10 14:19:14 -0800

    Delete src directory

--- src/Images.java
+++
@@ -1,8 +0,0 @@
-public class Images {
-    public static final String STARTING_SCREEN = "C:\\Users\\EJD77\\Desktop\\CrazyGolf\\Images\\StartingScreen.jpg";
-    public static final String STARTING_SCREEN_ACTIVE = "C:\\Users\\EJD77\\Desktop\\CrazyGolf\\Images\\StartingScreenActive.jpg";
-    public static final String HOLE_BACKGROUND = "C:\\Users\\EJD77\\Desktop\\CrazyGolf\\Images\\Hole.jpg";
-    public static final String BALL = "C:\\Users\\EJD77\\Desktop\\CrazyGolf\\Images\\Ball.png";
-    public static final String TRUMP = "C:\\Users\\EJD77\\Desktop\\CrazyGolf\\Images\\Trump.png";
-    public static final String ARROW = "C:\\Users\\EJD77\\Desktop\\CrazyGolf\\Images\\Arrow.png";
-}
