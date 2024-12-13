5df087816ede3d8b2ecc08fe0413f508b9f2bece
EddieDevine <edevine2025@cchsdons.com>
2024-12-12 17:38:50 -0800

    Delete src directory

--- src/Images.java
+++
@@ -1,9 +0,0 @@
-public class Images {
-    public static final String STARTING_SCREEN = "Images/StartingScreen.jpg";
-    public static final String STARTING_SCREEN_ACTIVE = "Images/StartingScreenActive.jpg";
-    public static final String HOLE_BACKGROUND = "Images/Hole.jpg";
-    public static final String BALL = "Images/Ball.png";
-    public static final String TRUMP = "Images/Trump.png";
-    public static final String ARROW = "Images/Arrow.png";
-    public static final String UP_ARROW = "Images/UpArrow.png";
-}
